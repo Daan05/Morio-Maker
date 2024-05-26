a07eb681b2537c2f85b4fc8b48139f28278e9552
WurmWillem <84849889+WurmWillem@users.noreply.github.com>
2024-05-26 15:35:19 +0200

    yes

--- app/Program.cs
+++ app/Program.cs
@@ -1,31 +1,81 @@
-﻿using Raylib_cs;
+﻿/*******************************************************************************************
+*
+*   raylib [core] example - Keyboard input
+*
+*   This example has been created using raylib 1.0 (www.raylib.com)
+*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
+*
+*   Copyright (c) 2014 Ramon Santamaria (@raysan5)
+*
+********************************************************************************************/
 
-namespace MorioMaker
+using System.Numerics;
+using Raylib_cs;
+using static Raylib_cs.Raylib;
+
+namespace Examples.Core;
+
+public class InputKeys
 {
-    public class Program
+    public static int Main()
     {
-        public static int Main()
+        
+        // Initialization
+        //--------------------------------------------------------------------------------------
+        const int screenWidth = 800;
+        const int screenHeight = 450;
+
+        InitWindow(screenWidth, screenHeight, "raylib [core] example - keyboard input");
+
+        Vector2 ballPosition = new((float)screenWidth / 2, (float)screenHeight / 2);
+
+        SetTargetFPS(60);       // Set target frames-per-second
+        //--------------------------------------------------------------------------------------
+
+        // Main game loop
+        while (!WindowShouldClose())
         {
-            const int screenWidth = 800;
-            const int screenHeight = 450;
-
-            Raylib.InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
-
-            Raylib.SetTargetFPS(60);
-
-            while (!Raylib.WindowShouldClose())
+            // Update
+            //----------------------------------------------------------------------------------
+            if (IsKeyDown(KeyboardKey.Right))
             {
-                Raylib.BeginDrawing();
-                Raylib.ClearBackground(Color.RayWhite);
-
-                Raylib.DrawText("Congrats! You created your first window!", 190, 200, 20, Color.Maroon);
-
-                Raylib.EndDrawing();
+                ballPosition.X += 2.0f;
             }
 
-            Raylib.CloseWindow();
+            if (IsKeyDown(KeyboardKey.Left))
+            {
+                ballPosition.X -= 2.0f; //fdsfas
+            }
 
-            return 0;
+            if (IsKeyDown(KeyboardKey.Up))
+            {
+                ballPosition.Y -= 2.0f;
+            }
+
+            if (IsKeyDown(KeyboardKey.Down))
+            {
+                ballPosition.Y += 2.0f;
+            }
+            //----------------------------------------------------------------------------------
+
+            // Draw
+            //----------------------------------------------------------------------------------
+            BeginDrawing();
+            ClearBackground(Color.RayWhite);
+
+            DrawText("move the ball with arrow keys", 10, 10, 20, Color.DarkGray);
+
+            DrawCircleV(ballPosition, 50, Color.Maroon);
+
+            EndDrawing();
+            //----------------------------------------------------------------------------------
         }
+
+        // De-Initialization
+        //--------------------------------------------------------------------------------------
+        CloseWindow();
+        //--------------------------------------------------------------------------------------
+
+        return 0;
     }
 }
