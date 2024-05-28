20fcc7805317748e789efcd1163910e3803e654c
WurmWillem <84849889+WurmWillem@users.noreply.github.com>
2024-05-28 12:24:52 +0200

    it's working

--- app/Program.cs
+++ app/Program.cs
@@ -7,118 +7,28 @@ namespace MorioMaker;
 
 public class Program
 {
-    const int gridSizeX = 26;
-    const int gridSizeY = 18;
+    // these consts are defined twice, should be fixed
+    public const int ScreenWidth = 1200;
+    public const int ScreenHeight = 816;
 
     public static int Main()
     {
         // Initialization
         //--------------------------------------------------------------------------------------
-        const int screenWidth = 1200;
-        const int screenHeight = 800;
 
-        const int gridSizeX = 60;
-        const int gridSizeY = 20;
-        const float blockSize = 48.0f;
 
-        const int mapWidth = (int)(gridSizeX * blockSize);
-        const int mapHeight = (int)(gridSizeY * blockSize);
-
-        InitWindow(screenWidth, screenHeight, "Morio Maker");
-        Image windowIcon = LoadImage("assets/morio.png"); 
+        InitWindow(ScreenWidth, ScreenHeight, "Morio Maker");
+        Image windowIcon = LoadImage("assets/morio.png");
         SetWindowIcon(windowIcon);
-
         SetTargetFPS(60);       // Set target frames-per-second
 
-        Texture2D blocks = LoadTexture("assets/blocks.png");
-        Rectangle[] blockTextureSourceRects = { 
-            new Rectangle(48.0f, 0.0f, 16.0f, 16.0f)
-        };
-
-        Tile[,] tiles = new Tile[gridSizeY, gridSizeX];
-        for (int j = 0; j < gridSizeY; j++) {
-               for (int i = 0; i < gridSizeX; i++) {
-                if (j > 14) {
-                    tiles[j,i] = new Tile(TileType.Block);
-                } else {
-                    tiles[j,i] = new Tile(TileType.Empty);
-                }
-            } 
-        }
-
-        Vector2 mapPos = new Vector2(720, 400);
-
         //--------------------------------------------------------------------------------------
-
-        // Initialization
-        //--------------------------------------------------------------------------------------
-        const int screenWidth = 1200;
-        const int screenHeight = 816;
-
-
-        InitWindow(screenWidth, screenHeight, "Morio Maker");
-
-        Image windowIcon = LoadImage("assets/morio.png"); 
-        SetWindowIcon(windowIcon);
-
-        SetTargetFPS(60);
 
         Game game = new Game();
 
-
-        // Main game loop
+        // Main loop
         while (!WindowShouldClose())
         {
-            // Update
-
-            //----------------------------------------------------------------------------------
-            if (IsKeyDown(KeyboardKey.Right))
-            {
-                // Stay in bounds of map
-                if (mapPos.X > 13.0f * blockSize) {
-                    mapPos.X -= 2.0f;
-                }
-            }
-
-            if (IsKeyDown(KeyboardKey.Left))
-            {
-                // Stay in bounds of map
-                if (mapPos.X < mapWidth - 13.0f * blockSize) {
-                    mapPos.X += 2.0f;
-                }
-            }
-
-            if (IsKeyDown(KeyboardKey.Up))
-            {
-                mapPos.Y -= 2.0f;
-            }
-
-            if (IsKeyDown(KeyboardKey.Down))
-            {
-                mapPos.Y += 2.0f;
-            }
-            //----------------------------------------------------------------------------------
-
-            // Draw
-            //----------------------------------------------------------------------------------
-            BeginDrawing();
-            ClearBackground(Color.RayWhite);
-
-            for (int j = 0; j < gridSizeY; j++) {
-               for (int i = 0; i < gridSizeX; i++) {
-
-                    // Loop through all tiles
-                    // Skip if the tile is empty
-                    if (tiles[j, i].type.GetHashCode() == -1) {
-                        continue;
-                    }
-                    
-                    Vector2 pos = new Vector2(i * blockSize, j * blockSize);
-                    //              tex         src                                                         dst                                                 origin              rot        tint
-                    DrawTexturePro(blocks, blockTextureSourceRects[tiles[j, i].type.GetHashCode()], new Rectangle(pos, new Vector2(blockSize, blockSize)), new Vector2(0.0f, 0.0f), 0.0f, Color.RayWhite );
-                }
-            } 
-        
             DrawFPS(10, 10);
 
             game.Update();
@@ -142,16 +52,19 @@ public class Program
 
 }
 
-class Tile {
+class Tile
+{
     public TileType type = TileType.Empty;
 
-    public Tile(TileType _type) {
+    public Tile(TileType _type)
+    {
         type = _type;
 
     }
 }
 
-enum TileType {
+enum TileType
+{
     Empty = -1,
     Block = 0,
 }
@@ -159,64 +72,82 @@ enum TileType {
 
 class Game
 {
+    // these consts are defined twice, should be fixed
+    public const int ScreenWidth = 1200; // these consts get exact integers when you divide by 48
+    public const int ScreenHeight = 816;
+
+    const float BlockSize = 48.0f;
+    const int GridSizeX = ScreenWidth / (int)BlockSize;
+    const int GridSizeY = ScreenHeight / (int)BlockSize;
+
+    const int MapWidth = (int)(GridSizeX * BlockSize);
+    const int MapHeight = (int)(GridSizeY * BlockSize);
+
     Tile[,] tiles;
     Morio morio;
-    Texture2D block_tex;
-    
+    Texture2D blocksTex;
+    Rectangle[] blockTextureSourceRects = {
+            new(48.0f, 0.0f, 16.0f, 16.0f)
+        };
+
     public Game()
     {
-        Tile[,] _tiles = new Tile[gridSizeY, gridSizeX];
-        for (int j = 0; j < gridSizeY; j++)
+        Tile[,] _tiles = new Tile[GridSizeY, GridSizeX];
+        for (int j = 0; j < GridSizeY; j++)
         {
-            for (int i = 0; i < gridSizeX; i++)
+            for (int i = 0; i < GridSizeX; i++)
             {
-                if (gridSizeY - j <= 2)
+                if (GridSizeY - j <= 2)
                 {
                     _tiles[j, i] = new Tile(TileType.Block);
-               }
+                }
                 else
                 {
                     _tiles[j, i] = new Tile(TileType.Empty);
                 }
             }
-          }
-      
+        }
+
         tiles = _tiles;
         morio = new Morio();
-        block_tex = LoadTexture("assets/blocks.png");
+        blocksTex = LoadTexture("assets/blocks.png");
+
     }
-    
+
     public void Update()
     {
-         morio.Update();
+        morio.Update();
     }
-    
+
     public void Render()
     {
-        Rectangle src = new(48.0f, 0.0f, 16.0f, 16.0f);
-        Vector2 origin = new Vector2(48.0f, 48.0f);
-        for (int j = 0; j < gridSizeY; j++)
+        Vector2 origin = new Vector2(BlockSize, BlockSize);
+        for (int j = 0; j < GridSizeY; j++)
         {
-            for (int i = 0; i < gridSizeX; i++)
+            for (int i = 0; i < GridSizeX; i++)
             {
-                if (tiles[j, i].type == TileType.Block)
+                if (tiles[j, i].type.GetHashCode() == -1)
                 {
-                    Vector2 pos = new Vector2(i * 48.0f, j * 48.0f);
-                    Rectangle dest = new Rectangle(pos, origin);
-                    DrawTexturePro(block_tex, src, dest, origin, 0.0f, Color.RayWhite);
+                    continue;
                 }
+
+                Vector2 pos = new(i * BlockSize, j * BlockSize);
+                Rectangle src = blockTextureSourceRects[tiles[j, i].type.GetHashCode()];
+                Rectangle dest = new(pos, origin);
+
+                DrawTexturePro(blocksTex, src, dest, origin, 0.0f, Color.RayWhite);
             }
         }
         morio.Render();
-     }
- }
+    }
+}
 
 class Morio
 {
     Texture2D tex;
     int x; // these probably need to be converted to floats
     int y;
-    
+
     const int Speed = 5;
 
     public Morio()
